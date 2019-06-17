6c8b75566c1f5129c11b9173d60a4f1404c713dc
Miguel Vaz <migueljvaz@gmail.com>
2019-06-17 17:26:54 +0200

    processing pdf

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,34 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import HTMLConverter,TextConverter,XMLConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import os
+
+def convert_pdf_to_txt(path_to_file):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    with open(path_to_file, 'rb') as fp:
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos=set()
+
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+            interpreter.process_page(page)
+        text = retstr.getvalue()
+
+    device.close()
+    retstr.close()
+    return text
+
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,7 +42,14 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
+    logger.info(input_filepath)
+    logger.info(output_filepath)
+    for x in os.listdir(input_filepath):
+        x_ = os.path.join(input_filepath, x)
+        if not os.path.isfile(x_) or not x_.endswith('.pdf'): continue
+        txt = convert_pdf_to_txt(x_)
+        with open(os.path.join(output_filepath, x + '.txt'), 'w+') as f:
+            f.write(txt)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
