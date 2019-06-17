964a959ae5c3ff58860de6c198b3fee1c97f4267
Miguel Vaz <migueljvaz@gmail.com>
2019-06-17 17:42:12 +0200

    added spacy and buba documents

---
+++ get_docs.sh
@@ -0,0 +1,8 @@
+wget https://www.ecb.europa.eu/paym/intro/publications/pdf/ecb.miptopical190604.en.pdf?ba6e962120fe85b3f06420fb297ee61f
+wget https://www.ecb.europa.eu/pub/pdf/other/ecb.stella_project_report_september_2017.pdf
+wget https://www.ecb.europa.eu/pub/pdf/other/stella_project_report_march_2018.pdf
+
+#wget https://www.bundesbank.de/resource/blob/665446/cfd6e8fbe0f2563b9fc1f48fabda8ca2/mL/2017-09-distributed-ledger-technologien-data.pdf
+wget https://www.bundesbank.de/resource/blob/707710/3f3bd66e8c8a0fbeb745886b3f072b15/mL/2017-09-distributed-data.pdf
+wget https://www.bundesbank.de/resource/blob/766672/29feab3f9079540441e3abda1ed2d2c1/mL/2018-10-25-blockbaster-final-report-data.pdf
+
